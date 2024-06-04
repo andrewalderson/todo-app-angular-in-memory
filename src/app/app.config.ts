a503d1e87a7bb83f26f5dc53e31d6466a0812f35
Andrew Alderson <andrew.alderson@outlook.com>
2024-06-04 13:20:15 -0600

    add and configure angular material

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,26 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
-
-import { routes } from './app.routes';
+import {
+  ApplicationConfig,
+  ENVIRONMENT_INITIALIZER,
+  inject,
+  provideZoneChangeDetection,
+} from "@angular/core";
+import { MatIconRegistry } from "@angular/material/icon";
+import { provideAnimationsAsync } from "@angular/platform-browser/animations/async";
+import { provideRouter } from "@angular/router";
+import { routes } from "./app.routes";
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideAnimationsAsync(),
+    {
+      provide: ENVIRONMENT_INITIALIZER,
+      useFactory: () => () =>
+        inject(MatIconRegistry).setDefaultFontSetClass(
+          "material-icons-outlined"
+        ),
+      multi: true,
+    },
+  ],
 };
