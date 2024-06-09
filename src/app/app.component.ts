1796dc5a475ab5d8c676818f93ad60f41d146c73
Andrew Alderson <andrew.alderson@outlook.com>
2024-06-09 16:26:06 -0600

    add app banner with logo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from "@angular/core";
+import { MatToolbar } from "@angular/material/toolbar";
 import { RouterOutlet } from "@angular/router";
+import { LogoComponent } from "./core/layout/logo/logo.component";
 
 @Component({
   selector: "todo-root",
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatToolbar, LogoComponent],
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.scss"],
 })
