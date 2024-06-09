e402acca466cfdbc2da023e6f05437be191c1315
Andrew Alderson <andrew.alderson@outlook.com>
2024-06-09 16:21:08 -0600

    move logo component to dedicated folder

--- src/app/shell/shell.component.ts
+++ src/app/shell/shell.component.ts
@@ -1,14 +1,14 @@
-import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
-import { MatButtonModule } from '@angular/material/button';
-import { MatIconModule } from '@angular/material/icon';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { RouterModule } from '@angular/router';
-import { IS_MOBILE_SCREEN } from '../app.tokens';
-import { LogoComponent } from './logo.component';
+import { ChangeDetectionStrategy, Component, inject } from "@angular/core";
+import { MatButtonModule } from "@angular/material/button";
+import { MatIconModule } from "@angular/material/icon";
+import { MatSidenavModule } from "@angular/material/sidenav";
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { RouterModule } from "@angular/router";
+import { IS_MOBILE_SCREEN } from "../app.tokens";
+import { LogoComponent } from "../core/layout/logo/logo.component";
 
 @Component({
-  selector: 'todo-shell',
+  selector: "todo-shell",
   standalone: true,
   imports: [
     MatSidenavModule,
@@ -18,9 +18,9 @@ import { LogoComponent } from './logo.component';
     RouterModule,
     LogoComponent,
   ],
-  templateUrl: './shell.component.html',
-  styleUrl: './shell.component.scss',
-  changeDetection: ChangeDetectionStrategy.OnPush
+  templateUrl: "./shell.component.html",
+  styleUrl: "./shell.component.scss",
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ShellComponent {
   protected isMobileScreen = inject(IS_MOBILE_SCREEN);
