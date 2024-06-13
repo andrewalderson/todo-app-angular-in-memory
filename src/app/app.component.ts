cd9cee585b054bfc0b00ffef6b397929c2ab2d01
Andrew Alderson <andrew.alderson@outlook.com>
2024-06-12 19:14:50 -0600

    implement account menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,24 @@
-import { Component } from "@angular/core";
+import { Component, inject } from "@angular/core";
+import { MatButton } from "@angular/material/button";
 import { MatToolbar } from "@angular/material/toolbar";
 import { RouterOutlet } from "@angular/router";
+import { AuthenticationService } from "./auth/services/authentication.service";
 import { LogoComponent } from "./core/layout/logo/logo.component";
+import { AccountMenuComponent } from "./features/account/account-menu/account-menu.component";
 
 @Component({
   selector: "todo-root",
   standalone: true,
-  imports: [RouterOutlet, MatToolbar, LogoComponent],
+  imports: [
+    RouterOutlet,
+    MatToolbar,
+    LogoComponent,
+    AccountMenuComponent,
+    MatButton,
+  ],
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.scss"],
 })
-export class AppComponent {}
+export class AppComponent {
+  protected readonly authenticationService = inject(AuthenticationService);
+}
