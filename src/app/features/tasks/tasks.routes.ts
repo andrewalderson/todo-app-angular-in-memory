43ab97497c4ff9d94d4f05af11b0b7b99b3e94ef
Andrew Alderson <andrew.alderson@outlook.com>
2024-06-13 12:24:21 -0600

    add initial task list

--- src/app/features/tasks/tasks.routes.ts
+++ src/app/features/tasks/tasks.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from "@angular/router";
+import { TaskListComponent } from "./features/task-list/task-list.component";
 import { TasksPage } from "./tasks.page";
 
 export const routes: Routes = [
   {
     path: "",
     component: TasksPage,
+    children: [{ path: "", component: TaskListComponent }],
   },
 ];
