385b678863c20f3e52458a03ca38b121242a4057
Andrew Alderson <andrew.alderson@outlook.com>
2024-06-26 13:57:44 -0600

    implememt listing of tasks

---
+++ src/app/features/tasks/features/task-list/services/task-list.store.ts
@@ -0,0 +1,14 @@
+import { Injectable, inject } from "@angular/core";
+import { toSignal } from "@angular/core/rxjs-interop";
+import { map } from "rxjs";
+import { TasksClient } from "../../../../../api/tasks/tasks.client";
+
+@Injectable({ providedIn: "root" })
+export class TaskListStore {
+  #tasksClient = inject(TasksClient);
+
+  readonly tasks = toSignal(
+    this.#tasksClient.getTasks().pipe(map((tasks) => tasks.value)),
+    { initialValue: [] }
+  );
+}
--- src/app/features/tasks/features/task-list/task-list.component.ts
+++ src/app/features/tasks/features/task-list/task-list.component.ts
@@ -1,13 +1,22 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  HostBinding,
+  inject,
+} from "@angular/core";
+import { TaskListItemComponent } from "./components/task-list-item/task-list-item.component";
+import { TaskListStore } from "./services/task-list.store";
 
 @Component({
-  selector: 'todo-task-list',
+  selector: "todo-task-list",
   standalone: true,
-  imports: [],
-  templateUrl: './task-list.component.html',
-  styleUrl: './task-list.component.scss',
-  changeDetection: ChangeDetectionStrategy.OnPush
+  imports: [TaskListItemComponent],
+  templateUrl: "./task-list.component.html",
+  styleUrl: "./task-list.component.scss",
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class TaskListComponent {
+  @HostBinding("role") role = "list";
 
+  protected readonly taskListStore = inject(TaskListStore);
 }
