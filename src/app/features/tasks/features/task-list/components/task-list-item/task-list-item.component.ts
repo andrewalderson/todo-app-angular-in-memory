238d9687a4fb7f4667b256b6ed700d721751a2ac
Andrew Alderson <andrew.alderson@outlook.com>
2024-06-26 13:56:26 -0600

    add initial task list item component
    This component currently only displays the title.

---
+++ src/app/features/tasks/features/task-list/components/task-list-item/task-list-item.component.ts
@@ -0,0 +1,21 @@
+import {
+  ChangeDetectionStrategy,
+  Component,
+  HostBinding,
+  input,
+} from "@angular/core";
+import { Task } from "../../../../../../api/tasks/tasks.client";
+
+@Component({
+  selector: "todo-task-list-item",
+  standalone: true,
+  imports: [],
+  templateUrl: "./task-list-item.component.html",
+  styleUrl: "./task-list-item.component.scss",
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class TaskListItemComponent {
+  @HostBinding("role") role = "listitem";
+
+  task = input.required<Task>();
+}
